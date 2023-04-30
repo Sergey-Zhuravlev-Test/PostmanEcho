03af4122b27124dd65f0b024a1d6780d7a5b9c32
Sergey <zhuravlevsergey7@gmail.com>
2023-04-30 17:40:07 +0300

    Test for fail

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PostmanEcho
+[![Build status](https://ci.appveyor.com/api/projects/status/il3tnc4d7jrqt1fr?svg=true)](https://ci.appveyor.com/project/Sergey-Zhuravlev-Test/postmanecho)
--- src/test/java/ru/netology/TestPostmanEcho.java
+++ src/test/java/ru/netology/TestPostmanEcho.java
@@ -19,6 +19,6 @@ public class TestPostmanEcho {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Postman Echo Task"));
+                .body("data", equalTo("Postman Echo Task Failed"));
     }
 }
